17ee5c6681bd6ca6d66f776a44014b1719784930
SowjanyaBawale <148544219+SowjanyaBawale@users.noreply.github.com>
2024-01-27 20:26:43 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,10 @@
+from math import pi
+
+print("hello world")
+print(2+4)
+print(23+45)
+print(23*45)
+print(pi)
+
+import keyword
+print(keyword, keyword.kwlist)
---
+++ pgm2.py
@@ -0,0 +1,9 @@
+X=input("Enter First name:")
+Y=input("Enter last name:")#string user input
+Z=input("age:")#int
+print(X,Y,"age is",Z)
+print(len(X)) #length of string
+print(Y[0])#index
+
+#string is immutability
+
